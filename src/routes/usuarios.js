5ceded904b2b297fd6802f8e9d4054f984334578
Antonio - TI (113) <ti2.m@concresul.com>
2026-05-20 15:57:41 -0300

    Atualização dos usuários

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const usuarios = Router();
 
 usuarios.get('/usuarios', (req,res)=>{
-    res.send('acessou a rota de usuários');
+    res.send('Esse acesso refere aos usuários');
 })
 
 export default usuarios;
